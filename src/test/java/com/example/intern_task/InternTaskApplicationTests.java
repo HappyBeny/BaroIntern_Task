3ab13cda9e298fa5a2100eabb7220fefa6048f6f
Happy Beny <qkdehdgur3@gmail.com>
2025-06-12 06:47:16 +0900

    ✨Feature : 프로젝í

---
+++ src/test/java/com/example/intern_task/InternTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intern_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
